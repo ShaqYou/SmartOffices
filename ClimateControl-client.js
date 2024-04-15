b23d6a5ba5599513a67d20ac8f250ac2a6a83c83
shaqYou <shaqayeq.yousofi@gmail.com>
2024-04-15 17:47:19 +0100

    adding functions to Climate control client

--- ClimateControl-client.js
+++ ClimateControl-client.js
@@ -7,3 +7,45 @@ const readline = require('readline');
 const packageDefinition = protoLoader.loadSync('SmartOffice.proto');
 const SmartOfficeProto = grpc.loadPackageDefinition(packageDefinition).SmartOffice;
 const client = new climateControlProto('localhost:50051', grpc.credentials.createInsecure());
+
+//Creating an interface for user Input
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  function askQuestion(query) {
+    return new Promise(resolve => rl.question(query, resolve));
+  }
+
+
+// Main function to interact with the Climate Control server
+async function main() {
+    // Adjust temperature
+    const targetTemperature = await askQuestion('Enter target temperature: ');
+    client.AdjustTemperature({ targetTemperature: parseFloat(targetTemperature) }, (error, response) => {
+        if (error) {
+            console.error(error);
+        } else {
+            console.log('AdjustTemperature Response:', response.status);
+            // After adjusting temperature, call adjustHumidity
+            adjustHumidity();
+        }
+    });
+}
+
+// Adjust humidity
+async function adjustHumidity() {
+    const humidityLevel = await askQuestion('Enter airflow level: ');
+    client.AdjustHumidity({ humidityLevel: parseInt(humidityLevel) }, (error, response) => {
+        if (error) {
+            console.error(error);
+        } else {
+            console.log('AdjustHumidity Response:', response.status);
+            // Close the readline interface after all interactions are complete
+            rl.close();
+        }
+    });
+}
+   main();
+                                                                      
