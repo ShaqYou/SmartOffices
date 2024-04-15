67f282a35154ab552538b5291aa133923c8cf5d3
shaqYou <shaqayeq.yousofi@gmail.com>
2024-04-15 16:50:19 +0100

    creating client for Climate Control

--- ClimateControl-client.js
+++ ClimateControl-client.js
@@ -2,3 +2,8 @@
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
 const readline = require('readline');
+
+//Creating Client
+const packageDefinition = protoLoader.loadSync('SmartOffice.proto');
+const SmartOfficeProto = grpc.loadPackageDefinition(packageDefinition).SmartOffice;
+const client = new climateControlProto('localhost:50051', grpc.credentials.createInsecure());
