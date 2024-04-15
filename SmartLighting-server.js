ef81d4d959e174daa3b3cdb26bf3479ea0182fc8
shaqYou <shaqayeq.yousofi@gmail.com>
2024-04-15 18:14:20 +0100

    methods for Smart Lighting server

--- SmartLighting-server.js
+++ SmartLighting-server.js
@@ -8,3 +8,20 @@ const smartOfficeProto = grpc.loadPackageDefinition(packageDefinition).smart_off
 
 //Creating a rpc server
 const server = new grpc.Server();
+
+//Implementing methods
+server.addService(smartOfficeProto.SmartOffice.service, {
+  ToggleLights: (call, callback) => {
+      const { on } = call.request;
+      const status = on ? 'Lights turned on' : 'Lights turned off';
+      console.log(status);
+      callback(null, { status });
+  },
+
+  AdjustBrightness: (call, callback) => {
+      const { brightnessLevel } = call.request;
+      const status = `Brightness adjusted to ${brightnessLevel}`;
+      console.log(status);
+      callback(null, { status });
+  },
+});
