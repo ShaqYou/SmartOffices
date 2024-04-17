904163b0f529ed94ba42e585702a5d5509a11081
shaqYou <shaqayeq.yousofi@gmail.com>
2024-04-17 22:26:16 +0100

    Creating grpc clients

--- SmartOfficeManager.js
+++ SmartOfficeManager.js
@@ -7,3 +7,16 @@ const readline = require('readline');
 const packageDefinition = protoLoader.loadSync('SmartOffice.proto');
 const smartOfficeProto = grpc.loadPackageDefinition(packageDefinition).smart_office;
 
+// Create gRPC clients services
+const lightingClient = new smartOfficeProto.SmartLighting('localhost:50051', grpc.credentials.createInsecure());
+const climateControlClient = new smartOfficeProto.ClimateControl('localhost:50052', grpc.credentials.createInsecure());
+const meetingRoomsClient = new smartOfficeProto.SmartMeetingRooms('localhost:50053', grpc.credentials.createInsecure());
+
+const userInput = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  function askQuestion(query) {
+    return new Promise(resolve => userInput.question(query, resolve));
+  }
