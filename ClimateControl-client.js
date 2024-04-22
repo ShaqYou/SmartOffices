a293bbb75ed94384151340643c8c0cc32aa4062f
shaqYou <shaqayeq.yousofi@gmail.com>
2024-04-22 16:27:23 +0100

    updating climateControl client

--- ClimateControl-client.js
+++ ClimateControl-client.js
@@ -4,48 +4,19 @@ const protoLoader = require('@grpc/proto-loader');
 const readline = require('readline');
 
 //Creating Client
-const packageDefinition = protoLoader.loadSync('SmartOffice.proto');
+const packageDefinition = protoLoader.loadSync('ClimateControl.proto');
 const SmartOfficeProto = grpc.loadPackageDefinition(packageDefinition).SmartOffice;
 const client = new climateControlProto('localhost:50051', grpc.credentials.createInsecure());
-
-//Creating an interface for user Input
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+const settingsStream = client.AdjustSettingsStream((error, response) => {
+    if (error) {
+      console.error('Error:', error);
+    } else {
+      console.log('SettingsResponse:', response.status);
+    }
   });
   
-  function askQuestion(query) {
-    return new Promise(resolve => rl.question(query, resolve));
-  }
-
-
-// Main function to interact with the Climate Control server
-async function main() {
-    // Adjust temperature
-    const targetTemperature = await askQuestion('Enter target temperature: ');
-    client.AdjustTemperature({ targetTemperature: parseFloat(targetTemperature) }, (error, response) => {
-        if (error) {
-            console.error(error);
-        } else {
-            console.log('AdjustTemperature Response:', response.status);
-            // After adjusting temperature, call adjustHumidity
-            adjustHumidity();
-        }
-    });
-}
-
-// Adjust humidity
-async function adjustHumidity() {
-    const humidityLevel = await askQuestion('Enter airflow level: ');
-    client.AdjustHumidity({ humidityLevel: parseInt(humidityLevel) }, (error, response) => {
-        if (error) {
-            console.error(error);
-        } else {
-            console.log('AdjustHumidity Response:', response.status);
-            // Close the readline interface after all interactions are complete
-            rl.close();
-        }
-    });
-}
-   main();
-                                                                      
+  // Sending multiple setting adjustment requests
+  settingsStream.write({ settingValue: 10 });
+  settingsStream.write({ settingValue: 20 });
+  settingsStream.write({ settingValue: 30 });
+  settingsStream.end();
