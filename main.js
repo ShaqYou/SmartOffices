6c94bf2a4b48d2dd248fded6118c5075a94d6bc2
shaqYou <shaqayeq.yousofi@gmail.com>
2024-04-22 18:14:54 +0100

    creating clients

--- main.js
+++ main.js
@@ -6,3 +6,8 @@ const readlineSync = require('readline-sync');
 const smartLightingProto = grpc.loadPackageDefinition(protoLoader.loadSync('proto/SmartLighting.proto')).smartoffice;
 const smartMeetingRoomsProto = grpc.loadPackageDefinition(protoLoader.loadSync('proto/SmartMeetingRooms.proto')).smartoffice;
 const climateControlProto = grpc.loadPackageDefinition(protoLoader.loadSync('proto/ClimateControl.proto')).smartoffice;
+
+// Create gRPC clients for each service
+const smartLightingClient = new smartLightingProto.SmartLighting('localhost:50051', grpc.credentials.createInsecure());
+const smartMeetingRoomsClient = new smartMeetingRoomsProto.SmartMeetingRooms('localhost:50052', grpc.credentials.createInsecure());
+const climateControlClient = new climateControlProto.ClimateControl('localhost:50053', grpc.credentials.createInsecure());
